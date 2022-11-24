51ccfba278da9ab299d85915581b1b6ff8022c24
Harsh Vardhan <harshv@pop-os.localdomain>
2022-11-24 11:33:04 +0530

    desktop details

--- routes/contents.py
+++ routes/contents.py
@@ -2,10 +2,14 @@ from flask import Blueprint, render_template
 
 contents = Blueprint('contents', __name__)
 
+
+from bin import vncs
+
 @contents.route('/dashboard')
 @contents.route('/')
 def dashboard():
-  return render_template('dashboard.html')
+  vncs_dict_info = vncs.list_all_vnc_server()
+  return render_template('dashboard.html', vncs_dict=vncs_dict_info['message'], status=vncs_dict_info['status'])
 
 @contents.route('/agents')
 def agents():
